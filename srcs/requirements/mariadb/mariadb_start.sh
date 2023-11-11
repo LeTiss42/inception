433d17aa2987d0b814fd1d9e77edd81788922d99
Mathis-lubuntu <ravera.mathis49@gmail.com>
2023-11-11 15:10:21 +0100

    bash script debut, infinite on exec mysqld_safe

---
+++ srcs/requirements/mariadb/mariadb_start.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# script bash to start mariadb service and setup the database
+# this can be done directly by hand through MariaDB monitor, for exemple :
+# $mariadb -u root -p to start         the MariaDB monitor
+# $SELECT user FROM mysql.user;        to display all user
+# $exit;                               to exit MariaDB monitor
+# but here we use a bash script to do it by itself
+
+service mariadb start
+
+# sleep 5 seconds to wait for other containers to start correctly
+sleep 5
+
+# we run MySQL commands via bash (here with mariadb) -> mariadb --help
+# -u user for login if not current user
+# -p to specify the password and avoid prompt
+# -e to execute the command and quit
+mariadb -u root -p"${MYSQL_ROOT_PASSWORD}" -e "CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;"
+mariadb -u root -p"${MYSQL_ROOT_PASSWORD}" -e "CREATE USER IF NOT EXISTS \`${MYSQL_USER}\`@'localhost' IDENTIFIED BY '${MYSQL_PASSWORD}';"
+mariadb -u root -p"${MYSQL_ROOT_PASSWORD}" -e "GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO \`${MYSQL_USER}\`@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
+# is next line really necessary?
+mariadb -u root -p"${MYSQL_ROOT_PASSWORD}" -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';"
+mariadb -u root -p"${MYSQL_ROOT_PASSWORD}" -e "FLUSH PRIVILEGES;"
+
+# mariadb-admin is an administration program go the mysqld daemon
+mariadb-admin -u root -p"${MYSQL_ROOT_PASSWORD}" shutdown
+
+# The mysqld_safe (or mariadb-safe but did not work here) startup script is in MariaDB distributions on Linux and Unix.
+# It is a wrapper that starts mysqld with some extra safety features.
+exec mysqld_safe
