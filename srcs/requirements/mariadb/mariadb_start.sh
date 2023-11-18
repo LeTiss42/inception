e8a2ab9b2f775be247f2f875661c28216e67a1c3
LeTiss <mathis.ravera49@gmail.com>
2023-11-18 10:59:23 +0100

    small variable name change in script

--- srcs/requirements/mariadb/mariadb_start.sh
+++ srcs/requirements/mariadb/mariadb_start.sh
@@ -16,15 +16,15 @@ sleep 5
 # -u user for login if not current user
 # -p to specify the password and avoid prompt
 # -e to execute the command and quit
-mariadb -u root -p"${MYSQL_ROOT_PASSWORD}" -e "CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;"
-mariadb -u root -p"${MYSQL_ROOT_PASSWORD}" -e "CREATE USER IF NOT EXISTS \`${MYSQL_USER}\`@'localhost' IDENTIFIED BY '${MYSQL_PASSWORD}';"
-mariadb -u root -p"${MYSQL_ROOT_PASSWORD}" -e "GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO \`${MYSQL_USER}\`@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
+mariadb -u root -p"${MARIA_ROOT_PWD}" -e "CREATE DATABASE IF NOT EXISTS \`${MARIA_DB}\`;"
+mariadb -u root -p"${MARIA_ROOT_PWD}" -e "CREATE USER IF NOT EXISTS \`${MARIA_USER}\`@'localhost' IDENTIFIED BY '${MARIA_PWD}';"
+mariadb -u root -p"${MARIA_ROOT_PWD}" -e "GRANT ALL PRIVILEGES ON \`${MARIA_DB}\`.* TO \`${MARIA_USER}\`@'%' IDENTIFIED BY '${MARIA_PWD}';"
 # is next line really necessary?
-mariadb -u root -p"${MYSQL_ROOT_PASSWORD}" -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';"
-mariadb -u root -p"${MYSQL_ROOT_PASSWORD}" -e "FLUSH PRIVILEGES;"
+mariadb -u root -p"${MARIA_ROOT_PWD}" -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MARIA_ROOT_PWD}';"
+mariadb -u root -p"${MARIA_ROOT_PWD}" -e "FLUSH PRIVILEGES;"
 
 # mariadb-admin is an administration program for the mysqld daemon
-mariadb-admin -u root -p"${MYSQL_ROOT_PASSWORD}" shutdown
+mariadb-admin -u root -p"${MARIA_ROOT_PWD}" shutdown
 
 # The mysqld_safe (or mariadb-safe but did not work here) startup script is in MariaDB distributions on Linux and Unix.
 # It is a wrapper that starts mysqld with some extra safety features suche as restarting the server when an error occurs.
